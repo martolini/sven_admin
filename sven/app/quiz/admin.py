0d43f838d0881ed07ed8af8bc97def12c33083fa
Martin Skow Røed <msroed@gmail.com>
2014-02-22 22:37:27 +0100

    fix

--- sven/app/quiz/admin.py
+++ sven/app/quiz/admin.py
@@ -11,13 +11,18 @@ class QuestionAdmin(admin.ModelAdmin):
 		url = reverse('admin:%s_%s_change' %(obj.category._meta.app_label,  obj.category._meta.module_name),  args=[obj.category.id] )
   		return u'<a href="%s">%s</a>' %(url,  obj.category)
 
+  	def image(obj):
+  		return u'<a href="%s" target="_blank">%s</a>' % (obj.image.url, obj.image)
+
 	category.allow_tags = True
+	image.allow_tags = True
+	readyonly_fields = ('image')
 
 	fieldsets = [
 	('Category', {'fields': ['category']}),
 	('Question', {'fields': ['text', 'image']}),
 	]
-	list_display = ('text', category)
+	list_display = ('text', category, image)
 	search_fields = ['text']
 	inlines = [AnswerAdmin]
 
