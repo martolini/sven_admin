6ef89f3c802f2ab8b7b52b33ff274eb0ba06ea7e
Martin Skow Røed <msroed@gmail.com>
2014-02-22 22:06:16 +0100

    image not required

--- sven/app/quiz/models.py
+++ sven/app/quiz/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Question(models.Model):
 	text = models.TextField()
 	category = models.ForeignKey(Category)
-	image = models.ImageField(upload_to='photos')
+	image = models.ImageField(upload_to='photos', blank=True)
 
 	def __unicode__(self):
 		return self.text
