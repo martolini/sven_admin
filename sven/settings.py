d3751fad15e5d51ecef2345a054e32a06f4e0152
Martin Skow Røed <msroed@gmail.com>
2014-02-22 22:08:19 +0100

    added outh

--- sven/settings.py
+++ sven/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'sven.app',
     'sven.app.quiz',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
